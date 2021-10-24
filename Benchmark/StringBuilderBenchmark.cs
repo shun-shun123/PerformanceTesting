65707f20743efee28a32eaa1b427199adfc6e99a
shun-shun123 <shunsuke15vb@gmail.com>
2021-10-24 16:26:50 +0900

    changed

--- Benchmark/StringBuilderBenchmark.cs
+++ Benchmark/StringBuilderBenchmark.cs
@@ -6,20 +6,28 @@ namespace Benchmark
     [Config(typeof(BenchmarkConfig))]
     public class StringBuilderBenchmark
     {
+        private const int TRY_COUNT = 100;
+        
         private static string TemplateMessage => "Hello, World";
 
         [Benchmark]
         public void UseString()
         {
             string message = string.Empty;
-            message += TemplateMessage;
+            for (var i = 0; i < TRY_COUNT; i++)
+            {
+                message += TemplateMessage;
+            }
         }
 
         [Benchmark]
         public void UseStringBuilder()
         {
-            var stringBuilder = new StringBuilder(TemplateMessage.Length);
-            stringBuilder.Append(TemplateMessage);
+            var stringBuilder = new StringBuilder(TemplateMessage.Length * TRY_COUNT);
+            for (var i = 0; i < TRY_COUNT; i++)
+            {
+                stringBuilder.Append(TemplateMessage);
+            }
         }
     }
 }
